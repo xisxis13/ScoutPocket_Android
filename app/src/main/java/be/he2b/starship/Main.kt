9b597566da817ea2738996ccbaf1e4a945d2a675
62664 <62664@etu.he2b.be>
2025-09-25 08:45:31 +0200

    TD1 - 2. Variables val et var

--- app/src/main/java/be/he2b/starship/Main.kt
+++ app/src/main/java/be/he2b/starship/Main.kt
@@ -1,5 +1,13 @@
 package be.he2b.starship
 
 fun main() {
+
+    val name = "Nemo"
+    name = "Capt. Nemo" // erreur car un val ne peut pas être réassigné
+
+    var name2 = "Cooper"
+    name2 = "J. Cooper"
+
     println("Welcome to Starship!")
+
 }
