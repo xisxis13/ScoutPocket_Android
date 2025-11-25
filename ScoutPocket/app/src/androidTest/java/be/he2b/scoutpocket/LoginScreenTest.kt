791ad97b749abb3990623e8a9acef4edf9e5e35b
62664 <62664@etu.he2b.be>
2025-11-25 17:17:36 +0100

    add implementation tests

--- ScoutPocket/app/src/androidTest/java/be/he2b/scoutpocket/LoginScreenTest.kt
+++ ScoutPocket/app/src/androidTest/java/be/he2b/scoutpocket/LoginScreenTest.kt
@@ -1,17 +1,22 @@
 package be.he2b.scoutpocket
 
+import android.os.SystemClock
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import be.he2b.scoutpocket.ui.screens.LoginScreen
+import be.he2b.scoutpocket.ui.screens.MainScreen
 import be.he2b.scoutpocket.viewmodel.LoginViewModel
 import org.junit.Rule
 import org.junit.Test
@@ -25,36 +30,93 @@ class LoginScreenTest {
     private lateinit var navController: TestNavHostController
 
     @Test
-    fun invalidMailShowsError() {
+    fun invalidEmail_ShowsError() {
         composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current)
             navController.navigatorProvider.addNavigator(ComposeNavigator())
 
+            val loginViewModel = LoginViewModel()
+
             LoginScreen(
-                viewModel = LoginViewModel(),
-                navController = navController
+                viewModel = loginViewModel,
+                navController = navController,
             )
         }
 
-        composeTestRule.onNodeWithText("Email").performTextInput("testexample.com")
+        composeTestRule.onNodeWithText("Email").performTextInput("test@invalide")
         composeTestRule.onNode(hasText("Connexion") and hasClickAction()).performClick()
-        composeTestRule.onNodeWithText("invalid email").assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Email non valide").assertIsDisplayed()
     }
 
     @Test
-    fun validMailNavigateToMainScreen() {
+    fun badCredentials_ShowsAuthError() {
         composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current)
             navController.navigatorProvider.addNavigator(ComposeNavigator())
 
-            LoginScreen(
-                viewModel = LoginViewModel(),
-                navController = navController
-            )
+            val loginViewModel = LoginViewModel()
+
+            NavHost(
+                navController,
+                startDestination = Screens.Login.route
+            ) {
+                composable(Screens.Login.route) {
+                    LoginScreen(
+                        viewModel = loginViewModel,
+                        navController = navController
+                    )
+                }
+                composable(Screens.Main.route) {
+                    MainScreen()
+                }
+            }
         }
 
-        composeTestRule.onNodeWithText("Email").performTextInput("test@example.com")
+        composeTestRule.onNodeWithText("Email").performTextInput("test@he2b.be")
+        composeTestRule.onNodeWithText("Mot de passe").performTextInput("mauvais!!")
+
         composeTestRule.onNode(hasText("Connexion") and hasClickAction()).performClick()
-        composeTestRule.onNodeWithText("ESI logo").assertIsDisplayed()
+
+        SystemClock.sleep(3000)
+
+        composeTestRule.onNodeWithText("Mauvais login ou mot de passe").assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("ESI logo").assertDoesNotExist()
+    }
+
+    @Test
+    fun correctCredentials_NavigatesToMainScreen() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+
+            val loginViewModel = LoginViewModel()
+
+            NavHost(
+                navController,
+                startDestination = Screens.Login.route
+            ) {
+                composable(Screens.Login.route) {
+                    LoginScreen(
+                        viewModel = loginViewModel,
+                        navController = navController
+                    )
+                }
+                composable(Screens.Main.route) {
+                    MainScreen()
+                }
+            }
+        }
+
+        composeTestRule.onNodeWithText("Email").performTextInput("test@he2b.be")
+        composeTestRule.onNodeWithText("Mot de passe").performTextInput("dev5!!")
+
+        composeTestRule.onNode(hasText("Connexion") and hasClickAction()).performClick()
+
+        SystemClock.sleep(3000)
+
+        composeTestRule.onNodeWithContentDescription("ESI logo").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Mauvais login ou mot de passe").assertDoesNotExist()
     }
 }
